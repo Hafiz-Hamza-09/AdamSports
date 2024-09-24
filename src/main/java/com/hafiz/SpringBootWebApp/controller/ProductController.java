abc90cba0461e8894ba00a7829d48443221951b2
Hafiz Hamza <hafizhmz09@gmail.com>
2024-09-25 01:07:29 +0530

    ResponseEntity added - To return the HTTP Status

--- src/main/java/com/hafiz/SpringBootWebApp/controller/ProductController.java
+++ src/main/java/com/hafiz/SpringBootWebApp/controller/ProductController.java
@@ -3,8 +3,12 @@ package com.hafiz.SpringBootWebApp.controller;
 import com.hafiz.SpringBootWebApp.data.Products;
 import com.hafiz.SpringBootWebApp.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.http.HttpClient;
+import java.net.http.HttpResponse;
 import java.util.List;
 
 @RestController
@@ -16,14 +20,19 @@ public class ProductController {
 
     //Get All Products List
     @RequestMapping("/products")
-    public List<Products> getProducts(){
-        return service.getProducts();
+    public ResponseEntity<List<Products>>  getProducts(){
+        return new ResponseEntity<>(service.getProducts(), HttpStatus.OK);
     }
 
     //Get the specific Product by ProductId
     @GetMapping("/products/{productId}")
-    public Products getProductById(@PathVariable int productId){  //@PathVariable to map productId to "/products/{productId}"
-        return  service.getProductById(productId);
+    public ResponseEntity<Products> getProductById(@PathVariable int productId){  //@PathVariable to map productId to "/products/{productId}"
+        Products products = service.getProductById(productId);
+        if(products != null)
+            return  new ResponseEntity<>(products,HttpStatus.OK);
+        else
+            return  new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
     }
 
     @PostMapping("/products")
